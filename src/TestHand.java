cfbd4199e945ba19a27f242a01666bcdf0a45fdf
brankeye <brankeye@gmail.com>
2015-09-27 14:35:08 -0400

    Added test for addCard function for Hand.

--- src/TestHand.java
+++ src/TestHand.java
@@ -6,4 +6,17 @@ public class TestHand extends TestCase {
 	public TestHand(String testName) {
 		super(testName);
 	}
+	
+	public void testAddCard() {
+		Hand firstHand = new Hand();
+		assertEquals(1, firstHand.addCard("TwoHearts"));
+		assertEquals(1, firstHand.addCard("TwoSpades"));
+		assertEquals(0, firstHand.addCard("TwoHearts")); // no duplicates
+		assertEquals(0, firstHand.addCard("AceOfSpades")); // doesn't accept incorrect cards
+		
+		assertEquals(1, firstHand.addCard("TwoClubs"));
+		assertEquals(1, firstHand.addCard("TwoDiamonds"));
+		assertEquals(1, firstHand.addCard("ThreeClubs"));
+		assertEquals(0, firstHand.addCard("TwoClubs")); // adding more than 5 cards fails
+	}
 }
