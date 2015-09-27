47802199d224aa725c870010f4f713e4cb82c271
brankeye <brankeye@gmail.com>
2015-09-27 14:08:45 -0400

    Added storeRankSuit function for Card.

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,16 @@
 
 public class Card {
 
+	private String rank;
+	private String suit;
+	
+	// parse rankSuit into separate rank and suit, then return success or failure
+	public int storeRankSuit(String rankSuit) {
+		String[] rs = rankSuit.split("(?=\\p{Upper})");
+		
+		rank = rs[0];
+		suit = rs[1];
+		
+		return 1;
+	}
 }
