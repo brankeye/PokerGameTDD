61131afcfe0c304f9b791b495ea8e15605cc7de7
brankeye <brankeye@gmail.com>
2015-09-27 14:00:57 -0400

    Added test for storeRankSuit function.

--- src/TestCard.java
+++ src/TestCard.java
@@ -6,4 +6,16 @@ public class TestCard extends TestCase {
 	public TestCard(String testName) {
 		super(testName);
 	}
+	
+	// stores the rank and suit of a card from the string format "RankSuit"
+	public void testStoreRankSuit() {
+		Card cardTwoHearts = new Card();
+		assertEquals(1, cardTwoHearts.storeRankSuit("TwoHearts"));
+		assertEquals(0, cardTwoHearts.storeRankSuit("TwoThreeFour"));
+		assertEquals(0, cardTwoHearts.storeRankSuit("One"));
+		assertEquals(0, cardTwoHearts.storeRankSuit("BugHearts"));
+		assertEquals(0, cardTwoHearts.storeRankSuit("TwoLife"));
+		assertEquals(0, cardTwoHearts.storeRankSuit("01"));
+		assertEquals(0, cardTwoHearts.storeRankSuit(""));
+	}
 }
