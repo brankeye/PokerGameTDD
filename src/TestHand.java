2768edf34c18482f298d7ca474683cd99da2316e
brankeye <brankeye@gmail.com>
2015-09-27 15:13:07 -0400

    Added test for getSortedScoreList.

--- src/TestHand.java
+++ src/TestHand.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+
 import junit.framework.*;
 
 public class TestHand extends TestCase {
@@ -18,6 +20,21 @@ public class TestHand extends TestCase {
 		assertEquals(1, firstHand.addCard("TwoDiamonds"));
 		assertEquals(1, firstHand.addCard("ThreeClubs"));
 		assertEquals(0, firstHand.addCard("TwoClubs")); // adding more than 5 cards fails
+	}
+	
+	public void testGetSortedScoreList() {
+		Hand firstHand = new Hand();
+		firstHand.addCard("TwoHearts");
+		firstHand.addCard("ThreeHearts");
+		firstHand.addCard("FourHearts");
+		firstHand.addCard("FiveHearts");
+		firstHand.addCard("SixHearts");
+		ArrayList<Integer> scoreList = firstHand.getSortedScoreList();
+		assertEquals(1, scoreList.get(0)); // 2 Card has score of 1
+		assertEquals(2, scoreList.get(1)); // 3 Card has score of 2
+		assertEquals(3, scoreList.get(2)); // 4 Card has score of 3
+		assertEquals(4, scoreList.get(3)); // 5 Card has score of 4
+		assertEquals(5, scoreList.get(4)); // 6 Card has score of 5
 	}
 	
 	public void testGetHandScore() {
